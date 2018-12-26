ef671e3cb3be800e759145c23582f4835f282f2b
aawasthi <aawasthi@bhavnacorp.com>
2018-12-26 10:36:20 +0530

    Tiemsheet DTOs

---
+++ ETM.Models/Dto/TeamTasksDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETM.Repository.Dto
+{
+	public class TeamTasksDto
+	{
+		public int TeamId { get; set; }
+		public List<int> TaskIds { get; set; }
+	}
+}
---
+++ ETM.Models/Dto/UserDateDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETM.Repository.Dto
+{
+	public class UserDateDto
+	{
+		public int UserId { get; set; }
+		public DateTime Date { get; set; }
+	}
+}
